063f03005771ec130d77369b5e18387cc91dba30
abdulaziz <abdulazizabdu707@gmail.com>
2026-05-30 14:15:28 +0300

    first commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
+
 import AppTabs from '@/components/app-tabs';
 
 export default function TabLayout() {
