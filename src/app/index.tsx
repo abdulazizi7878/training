9b667debc4f872de5e9b82fb2a7a92308cf1e268
abdulaziz <abdulazizabdu707@gmail.com>
2026-05-30 15:18:12 +0300

    the real tutorial has started

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,21 @@
-import { View,Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function Home(){
-
-  return(
-
-    <View>
-      <Text>Hello World</Text>
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.myText}>Hello world</Text>
     </View>
-
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#010101",
+  },
+  myText: {
+    color: "#ffffff",
+  },
+});
